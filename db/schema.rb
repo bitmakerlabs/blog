1305553eec92e78490fe9507ce0c5db1bb468ee8
Julie Hache <elektrophile@gmail.com>
2014-06-23 09:18:04 -0400

    Add comments association

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140617155903) do
+ActiveRecord::Schema.define(version: 20140620153955) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.text     "body"
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "article_id"
   end
 
 end
