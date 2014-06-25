fb37217a65832fda16d6d24e5741cbcd806afc0d
Julie Hache <elektrophile@gmail.com>
2014-06-25 10:27:42 -0400

    Associate Comment and User models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140623153311) do
+ActiveRecord::Schema.define(version: 20140624153059) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140623153311) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "article_id"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
