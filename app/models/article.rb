64549569299a5ced53b552e7ed31a1f2c0c5c4f4
Julie Hache <elektrophile@gmail.com>
2014-06-25 10:24:26 -0400

    Add validation and form errors for Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,9 @@ class Article < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
+  validates :title, :text, presence: true
+  validates :title, length: { minimum: 6 }
+
 
   scope :latest, -> { order("created_at DESC").limit(5) }
 end
