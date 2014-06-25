480241f842eda46ca5bab3f6303e5c38441bbae7
Julie Hache <elektrophile@gmail.com>
2014-06-25 10:24:58 -0400

    Titleize title after validation with callback

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,11 @@ class Article < ActiveRecord::Base
   validates :title, :text, presence: true
   validates :title, length: { minimum: 6 }
 
+  after_validation :format_title
 
   scope :latest, -> { order("created_at DESC").limit(5) }
+
+  def format_title
+    self.title = title.titleize
+  end
 end
